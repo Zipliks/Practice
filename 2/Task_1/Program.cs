0b757b2a9f2b6e31376c9c384aae215366fc1524
Ethan Hansen <Zipliks@gmail.com>
2020-09-24 17:21:10 +0300

    Uploaded second practice

---
+++ 2/Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿#undef c
+using System;
+
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // C#
+            SByte a = 0;
+            Byte b = 0; 
+            Int16 c = 0; 
+            Int32 d = 0; 
+            System.Int64 e = 0; 
+            string s = "";
+            Exception ex = new Exception();
+
+            // C#
+            object[] types = { a, b, c, d, e, s, ex };
+
+            // C#
+            foreach (object _vars in types)
+            {
+                string type;
+                if (_vars.GetType().IsValueType) type = "Value type";
+                else
+                    type = "Reference Type";
+                Console.WriteLine("{0}: {1}", _vars.GetType(), type);
+            }
+        }
+    }
+}
---
+++ 2/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //c#
+            string s = "Microsoft .NET Framework 2 Application Development Foundation";
+            string[] sa = s.Split(' ');
+
+            Array.Sort(sa);
+            foreach (string item in sa)
+            {
+                Console.WriteLine(item);
+            }
+            s = string.Join(" ", sa); 
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
